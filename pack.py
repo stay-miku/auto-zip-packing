fde145c912825f5646d0ab3a2d55ac32041927bb
stay_miku <staymiku520@gmail.com>
2024-01-29 15:42:13 +0800

    init

--- pack.py
+++ pack.py
@@ -10,7 +10,7 @@ async def packing(source: str, destination: str, segment_size=-1, password=""):
     if repack_type == "7z":
         return_code = await execute(f'7z a {"-v" + str(segment_size) if segment_size > 0 else ""} {"-p" + password if password != "" else ""} -t7z -mx=0 -r "{destination}" "{source}"*')
     elif repack_type == "rar":
-        return_code = await execute(f'rar a -rr3 -scfc -r -k -ed -ms -htb -m0 {"-v" + str(segment_size) if segment_size > 0 else ""} -s -os -oi:65536 -qo+ -tk {"-p" + password if password != "" else ""} "{destination}" "{source}"*')
+        return_code = await execute(f'rar a -rr3 -scfc -r -k -ed -ms -htb -m0 {"-v" + str(segment_size) if segment_size > 0 else ""} -s -oi:65536 -qo+ -tk {"-p" + password if password != "" else ""} "{destination}" "{source}"*')
     else:
         raise Exception(f"unknown repack type: {repack_type}")
     if return_code != 0:
